773c920a2f59fc4bdd8a66a213234b2ac4310fb0
geass991155 <33750225+geass991155@users.noreply.github.com>
2021-02-20 17:22:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def handle_message(event):
         image_carousel_template = ImageCarouselTemplate(columns=[
             ImageCarouselColumn(image_url='https://i.imgur.com/ItFGIvU.jpg',
                                 action=URIAction(label='小番茄',
-                                uri='',
+                                uri='https://www.goodsmile.info/zh/product/7952/%E9%BB%8F%E5%9C%9F%E4%BA%BA+%E7%B6%A0%E9%96%93%E7%9C%9F%E5%A4%AA%E9%83%8E.html',
                                 )),
             ImageCarouselColumn(image_url='https://i.imgur.com/anKHYy9.jpg',
                                 action=URIAction(label='木瓜',
